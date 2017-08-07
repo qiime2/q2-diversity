e835e3e799f5c92c26c4642a824f574ef4e789c8
Max von Hippel <maxvonhippel1996@gmail.com>
2017-08-07 12:28:17 -0700

    Legend UI updating now except Select All but not updating the chart yet

--- q2_diversity/_alpha/alpha_rarefaction_assets/src/legend.js
+++ q2_diversity/_alpha/alpha_rarefaction_assets/src/legend.js
@@ -2,42 +2,43 @@ import { curData, toggle, toggleAll } from './data';
 
 export default function appendLegendKey(legend, entry, ly, c, color) {
   // line toggle in the legend
-  legend.append('rect')
-      .attr('id', `idrect${entry}`)
-      .attr('class', 'legend')
-      .attr('x', 0)
-      .attr('y', ly - 2.5)
-      .attr('width', 15)
-      .attr('height', 5)
-      .style('stroke', 'darkGrey')
-      .style('fill', () => curData[entry].line)
-      .on('click', () => {
-        const newColor = curData[entry].line === c ? 'white' : c;
-        if (entry === 'Select%20All') {
-          toggleAll(false, true, color);
-        } else {
-          toggle(entry, null, newColor);
-          this.style('fill', () => curData[entry].line);
-        }
-      });
+  const rect = legend.append('rect')
+    .data(curData[entry])
+    .attr('id', `idrect${entry}`)
+    .attr('class', 'legend')
+    .attr('x', 0)
+    .attr('y', ly - 2.5)
+    .attr('width', 15)
+    .attr('height', 5)
+    .style('stroke', 'darkGrey')
+    .style('fill', () => curData[entry].line);
+  rect.on('click', () => {
+    const newColor = curData[entry].line === c ? 'white' : c;
+    if (entry === 'Select%20All') {
+      toggleAll(false, true, color);
+    } else {
+      toggle(entry, null, newColor);
+      rect.style('fill', () => curData[entry].line);
+    }
+  });
   // dot toggle in the legend
-  legend.append('circle')
-      .attr('id', `iddot${entry}`)
-      .attr('class', 'legend')
-      .attr('cx', 30)
-      .attr('cy', ly)
-      .attr('r', 5)
-      .style('stroke', 'darkGrey')
-      .style('fill', () => curData[entry].dots)
-      .on('click', () => {
-        const newColor = curData[entry].dots === c ? 'white' : c;
-        if (entry === 'Select%20All') {
-          toggleAll(true, false, color);
-        } else {
-          toggle(entry, newColor, null);
-          this.style('fill', () => curData[entry].dots);
-        }
-      });
+  const dot = legend.append('circle')
+    .attr('id', `iddot${entry}`)
+    .attr('class', 'legend')
+    .attr('cx', 30)
+    .attr('cy', ly)
+    .attr('r', 5)
+    .style('stroke', 'darkGrey')
+    .style('fill', () => curData[entry].dots);
+  dot.on('click', () => {
+    const newColor = curData[entry].dots === c ? 'white' : c;
+    if (entry === 'Select%20All') {
+      toggleAll(true, false, color);
+    } else {
+      toggle(entry, newColor, null);
+      dot.style('fill', () => curData[entry].dots);
+    }
+  });
   // text for key in the legend
   legend.append('text')
       .attr('class', 'legend')
