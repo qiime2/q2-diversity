49261a8027f95311e80624614a1c2c37d4361245
Max von Hippel <maxvonhippel1996@gmail.com>
2017-08-08 15:26:53 -0700

    Less broken but not moving the chart or anything yet

--- q2_diversity/_alpha/alpha_rarefaction_assets/src/axis.js
+++ q2_diversity/_alpha/alpha_rarefaction_assets/src/axis.js
@@ -1,41 +1,26 @@
 export function setupXLabel(svg, width, height, label, xAxis) {
-  let maxLabelX = 30;
   svg.select('.x.axis')
     .attr('transform', `translate(0,${height})`)
-    .call(xAxis)
-      .selectAll('text')
-      .style('text-anchor', 'end')
-      .attr('dx', '-.8em')
-      .attr('dy', '-0.5em')
-      .attr('transform', function setHeight() {
-        const textHeight = this.getComputedTextLength();
-        if (textHeight > maxLabelX) maxLabelX = textHeight;
-        return 'rotate(-90)';
-      });
+    .call(xAxis);
 
-  svg.select('.x.label')
+  const l = svg.select('.x.label')
     .attr('text-anchor', 'middle')
     .style('font', '12px sans-serif')
-    .attr('transform', `translate(500,${(maxLabelX + height)})`)
+    .attr('transform', `translate(${(width / 2)},${(height + 40)})`)
     .text(label);
-  return maxLabelX;
+  const textHeight = l.node().getComputedTextLength();
+  return Math.max(textHeight, 30);
 }
 
 export function setupYLabel(svg, height, label, yAxis) {
-  let maxLabelY = 10;
   svg.select('.y.axis')
-    .call(yAxis)
-      .selectAll('text')
-      .attr('transform', function setHeight() {
-        const textHeight = this.getComputedTextLength();
-        if (textHeight > maxLabelY) maxLabelY = textHeight;
-        return '';
-      });
+    .call(yAxis);
 
-  svg.select('.y.label')
+  const l = svg.select('.y.label')
     .attr('text-anchor', 'middle')
     .style('font', '12px sans-serif')
-    .attr('transform', `translate(-${maxLabelY},${(height / 2)})rotate(-90)`)
+    .attr('transform', `translate(-50,${(height / 2)})rotate(-90)`)
     .text(label);
-  return maxLabelY;
+  const textHeight = l.node().getComputedTextLength();
+  return Math.max(textHeight, 30);
 }
