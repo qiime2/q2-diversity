e61d583911d460dbd1bcbc2c7591c8092c536dc1
Macabe Wood <macabewood@gmail.com>
2025-08-07 13:37:55 -0700

    Chaning plot type to hex in mantel

--- q2_diversity/_beta/_visualizer.py
+++ q2_diversity/_beta/_visualizer.py
@@ -325,7 +325,7 @@ def mantel(output_dir: str, dm1: skbio.DistanceMatrix,
     x = 'Pairwise Distance (%s)' % label1
     y = 'Pairwise Distance (%s)' % label2
     scatter_data = pd.DataFrame(scatter_data, columns=[x, y])
-    sns.regplot(x=x, y=y, data=scatter_data, fit_reg=False)
+    sns.jointplot(x=x, y=y, data=scatter_data, kind='hex')
     plt.savefig(os.path.join(output_dir, 'mantel-scatter.svg'))
     plt.close()
 
